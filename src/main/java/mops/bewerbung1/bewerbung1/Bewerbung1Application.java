82675c6cb34754192b08ced2b765bc21ad6c3793
AcnoZed <Me.ouss.am@outlook.de>
2020-03-03 09:19:47 +0100

    Initialize Project

---
+++ src/main/java/mops/bewerbung1/bewerbung1/Bewerbung1Application.java
@@ -0,0 +1,13 @@
+package mops.bewerbung1.bewerbung1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bewerbung1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bewerbung1Application.class, args);
+	}
+
+}
