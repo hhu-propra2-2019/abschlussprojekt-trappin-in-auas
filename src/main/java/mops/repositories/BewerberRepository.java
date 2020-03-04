5204705934b5c92e4fe9423d0051f3da52942492
LanPodder <lanpodder@localhost.localdomain>
2020-03-04 06:18:46 -0500

    added binary files to gitignore

--- src/main/java/mops/repositories/BewerberRepository.java
+++ src/main/java/mops/repositories/BewerberRepository.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import mops.models.Bewerber;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BewerberRepository extends CrudRepository<Bewerber, String> {
     Bewerber findBewerberByKennung(String kennung);
     List<Bewerber> findAll();
+    Bewerber save(Bewerber b);
+    void saveAll(List<Bewerber> b);
 }
