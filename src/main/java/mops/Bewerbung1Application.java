3f001b340eb2930b1d03045572933a05097d8da5
Jakob Handke <jakobhandke@icloud.com>
2020-03-04 09:59:58 +0100

    tabs -> spaces

--- src/main/java/mops/Bewerbung1Application.java
+++ src/main/java/mops/Bewerbung1Application.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Bewerbung1Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Bewerbung1Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Bewerbung1Application.class, args);
+  }
 
 }
