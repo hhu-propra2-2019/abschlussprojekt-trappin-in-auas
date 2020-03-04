799b33dabfabed23ea9dc9de1e53daf3614ebbf5
bayarabdullah <bayar1996@gmail.com>
2020-03-04 12:07:25 +0100

    added examples

--- src/main/java/mops/controllers/ExampleController.java
+++ src/main/java/mops/controllers/ExampleController.java
@@ -18,7 +18,11 @@ public class ExampleController{
 
     @GetMapping("/example")
     public String alle(){
-        Bewerber b = new Bewerber();
+        Bewerber b1 = new Bewerber("emjo100", "Johnson", "Emma", "23.04.2000");
+        Bewerber b2 = new Bewerber("uljo234", "Johanson", "Ulrich", "01.02.1999");
+        Bewerber b3 = new Bewerber("mahi932", "Hills", "Mark", "28.02.1997");
+        Bewerber b4 = new Bewerber("bewa232", "Watson", "Benjamin", "12.04.1998");
+        Bewerber b5 = new Bewerber("gewa101", "George", "Walter", "09.11.1998");
         List<Bewerber> alleB = bewerberRepository.findAll();
         alleB.stream().forEach(x -> System.out.println(x));
         return "example";
