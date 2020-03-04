3f9ec1cf02b1d53128f4cb174e7a11198edff544
bayarabdullah <bayar1996@gmail.com>
2020-03-04 12:18:06 +0100

    added the method save() and saveAll() to BewerberRepository

--- src/main/java/mops/repositories/BewerberRepository.java
+++ src/main/java/mops/repositories/BewerberRepository.java
@@ -8,4 +8,10 @@ import org.springframework.data.repository.CrudRepository;
 public interface BewerberRepository extends CrudRepository<Bewerber, String> {
     Bewerber findBewerberByKennung(String kennung);
     List<Bewerber> findAll();
+
+    @Override
+    <S extends Bewerber> S save(S entity);
+
+    @Override
+    <S extends Bewerber> Iterable<S> saveAll(Iterable<S> entities);
 }
