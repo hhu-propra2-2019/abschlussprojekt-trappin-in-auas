2b76eb1574fd985ba6808ecf13f72e7689826467
LanPodder <alan.i.shref@gmail.com>
2020-03-06 11:47:59 +0100

    added 'verteiltAn' column to bewerber and findAllNichtVerteilteBewerber to service

--- src/main/java/mops/domain/services/IBewerberService.java
+++ src/main/java/mops/domain/services/IBewerberService.java
@@ -1,9 +1,13 @@
 package mops.domain.services;
 
+import java.util.List;
+
 import mops.domain.models.Bewerber;
 
 public interface IBewerberService {
     void addBewerber(Bewerber b);
 
     Bewerber findBewerberByKennung(String kennung);
+    List<Bewerber> findAlleBewerber();
+    List<Bewerber> findAlleNichtVerteilteBewerber();
 }
