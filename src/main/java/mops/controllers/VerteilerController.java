08a194a3b26cd6bf71e7aa729f489e516b332cf8
LanPodder <alan.i.shref@gmail.com>
2020-03-06 09:56:02 +0100

    adde simple controller for distribution

--- src/main/java/mops/controllers/VerteilerController.java
+++ src/main/java/mops/controllers/VerteilerController.java
@@ -1,21 +1,29 @@
 package mops.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import mops.domain.models.Bewerber;
 import mops.services.BewerberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/verteiler")
 public class VerteilerController {
     
     @Autowired
     private BewerberService bewerberService;
 
-    @GetMapping("/verteiler")
+    @GetMapping("/uebersicht")
     public String verteilen(Model model){
-        return "";
+        List<Bewerber> alleBewerber = bewerberService.findAllBewerber();
+        System.out.println(alleBewerber);
+        model.addAttribute("bewererbungen", alleBewerber);
+        return "verteiler";
     }
     
 }
