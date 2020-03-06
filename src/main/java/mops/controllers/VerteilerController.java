6869a37b9402b9e3066aa34ddca3c6cb4e8c1bc2
LanPodder <alan.i.shref@gmail.com>
2020-03-06 09:14:06 +0100

    new task: write controller for distribution

---
+++ src/main/java/mops/controllers/VerteilerController.java
@@ -0,0 +1,13 @@
+package mops.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import mops.services.BewerberService;
+
+public class VerteilerController {
+    
+    @Autowired
+    private BewerberService bewerberService;
+
+    
+}
--- src/test/java/mops/bewerbung1/services/BewerberServiceTest.java
+++ src/test/java/mops/bewerbung1/services/BewerberServiceTest.java
@@ -1,40 +1,15 @@
 package mops.bewerbung1.services;
-import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
 
-import mops.domain.models.Bewerber;
-import mops.domain.repositories.BewerberRepository;
-import mops.services.BewerberService;
+
+
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 public class BewerberServiceTest {
 
-    private BewerberService bewerberService;
-
     @Before
     void setUp() {
-        this.bewerberService = mock(BewerberService.class);
+
     }
-
-    @Test
-    void saveBewerberTest(){
-        Bewerber b = new Bewerber("jocl100", "Clark", "John", "23.05.1999");
-        //ArgumentCaptor<Bewerber> valueCaptor = ArgumentCaptor.forClass(Bewerber.class);
-
-        //when(bewerberService.findBewerberByKennung(Mockito.anyString())).thenReturn(b);
-        //doNothing().when(bewerberService).addBewerber(valueCaptor.capture());
-
-        //bewerberService.addBewerber(b);
-        //verify(bewerberService).addBewerber(b);
-        //assertEquals(bewerberService.findBewerberByKennung("jocl100"), valueCaptor.getValue());
-    }
-
 }
