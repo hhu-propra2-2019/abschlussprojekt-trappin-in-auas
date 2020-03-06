84a7fc65e0ace8324c47a231b97a5a91438123b6
bayarabdullah <bayar1996@gmail.com>
2020-03-06 09:43:43 +0100

    verteiler Template nochmal commiten

--- src/main/java/mops/controllers/VerteilerController.java
+++ src/main/java/mops/controllers/VerteilerController.java
@@ -3,11 +3,19 @@ package mops.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import mops.services.BewerberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class VerteilerController {
     
     @Autowired
     private BewerberService bewerberService;
 
+    @GetMapping("/verteiler")
+    public String verteilen(Model model){
+        return "";
+    }
     
 }
