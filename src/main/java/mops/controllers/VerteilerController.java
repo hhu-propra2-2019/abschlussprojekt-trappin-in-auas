09d03068ae62fb2dea4ae0a8220b1130ee915697
LanPodder <alan.i.shref@gmail.com>
2020-03-10 11:36:39 +0100

    added interface for mappingservice

--- src/main/java/mops/controllers/VerteilerController.java
+++ src/main/java/mops/controllers/VerteilerController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/verteiler")
