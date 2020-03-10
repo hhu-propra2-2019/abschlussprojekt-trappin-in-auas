0225dfe355255716882196c21fbd846618517518
Jakob Handke <jakobhandke@icloud.com>
2020-03-10 11:34:15 +0100

    rename!

--- src/main/java/mops/domain/models/lehrstuhl/ModulListenElement.java
+++ src/main/java/mops/domain/models/lehrstuhl/ModulListenElement.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class ModulListe {
+public class ModulListenElement {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
