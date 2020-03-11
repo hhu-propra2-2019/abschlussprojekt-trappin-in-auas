cc32951fbe1d3a0a024781c9ccdb309ac8e1789f
Jakob Handke <jakobhandke@icloud.com>
2020-03-11 12:30:56 +0100

    add test, add admin role

--- src/test/java/mops/bewerbung1/AuthenticationManagerTest.java
+++ src/test/java/mops/bewerbung1/AuthenticationManagerTest.java
@@ -1,9 +1,72 @@
 package mops.bewerbung1;
 
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import mops.authentication.account.AuthenticationManager;
+import mops.authentication.account.keycloak.Account;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.mockito.internal.util.collections.Sets;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class AuthenticationManagerTest {
+@TestInstance(Lifecycle.PER_CLASS)
+class AuthenticationManagerTest {
 
+  Account orga;
+  Account student;
+  Account boss;
+  Account nobody;
+  Account admin;
+
+  @BeforeAll
+  void setUp() {
+    orga = new Account("Orgaknilch", "orga@mops", "image", Sets.newSet("ROLE_orga"));
+    student = new Account("Student", "student@mops", "image2", Sets.newSet("ROLE_studentin"));
+    boss = new Account("BigBoss", "boss@mops", "boss_image", Sets.newSet("ROLE_boss"));
+    nobody = new Account("Herr Niemand", "", "null", Sets.newSet());
+    admin = new Account("ADMIN", "admin@mops", "admimage", Sets.newSet("ROLE_admin"));
+  }
+
+// ORGA //
+  @Test
+  void testOrgaIsOrga() {
+    AuthenticationManager authManager = new AuthenticationManager(orga);
+    assertThat(authManager.isOrganizer()).isTrue();
+  }
+
+  @Test
+  void testOrgaIsNotStudent() {
+    AuthenticationManager authManager = new AuthenticationManager(orga);
+    assertThat(authManager.isStudent()).isFalse();
+  }
+
+  @Test
+  void testOrgaisNotAdmin() {
+    AuthenticationManager authManager = new AuthenticationManager(orga);
+    assertThat(authManager.isAdmin()).isFalse();
+  }
+
+  @Test
+  void testOrgaIsNotBoss() {
+    AuthenticationManager authManager = new AuthenticationManager(orga);
+    assertThat(authManager.isBoss()).isFalse();
+  }
+
+// ADMIN //
+  @Test
+  void testAdminIsAdmin() {
+    AuthenticationManager authManager = new AuthenticationManager(admin);
+    assertThat(authManager.isAdmin()).isTrue();
+  }
+
+  @Test
+  void testAdminIsNotOrga() {
+    AuthenticationManager authmanager = new AuthenticationManager(admin);
+    assertThat(authmanager.isOrganizer()).isFalse();
+  }
+
+  // ...
 }
