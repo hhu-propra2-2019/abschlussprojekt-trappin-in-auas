effe1341b57f1441786e78f21820c26a287873c9
Jakob Handke <jakobhandke@icloud.com>
2020-03-16 12:25:26 +0100

    update code style

---
+++ src/test/java/mops/services/BewerberServiceTest.java
@@ -0,0 +1,47 @@
+package mops.services;
+
+import static org.mockito.Mockito.mock;
+
+import mops.domain.database.dto.*;
+import mops.domain.models.*;
+import mops.domain.repositories.BewerberRepository;
+import mops.services.BewerberService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BewerberServiceTest {
+
+  transient private BewerberService bewerberService;
+  private BewerberRepository bewerberRepository;
+
+  @BeforeEach
+  void setUp() {
+    this.bewerberService = mock(BewerberService.class);
+
+  }
+
+  private transient String[] vornamen = {"Luca", "Kristine", "Rosemarie", "Melanie", "Maximilian"};
+  private transient String[] nachnamen = {"Tronke", "Ostermann", "Teubner", "Thoma", "Förstner"};
+  private transient String[] geburtstage = {"27.03.1998", "14.07.2000", "03.01.1999", "22.08.1999", "01.05.1997"};
+  private transient String[] verteilungen = {"Jens", "Golov", "Tratusch"};
+
+  private BewerberModel generateVerteilterBewerber() {
+    String vname = fromRandomPosition(vornamen);
+    String nname = fromRandomPosition(nachnamen);
+    String kennung = vname.substring(0, 3) + nname.substring(0, 3) + (int) Math.random()*999;
+    return new BewerberModel(kennung, nname, vname, fromRandomPosition(geburtstage), fromRandomPosition(verteilungen));
+  }
+
+  private BewerberModel generateNichtVerteilterBewerber() {
+    String vname = fromRandomPosition(vornamen);
+    String nname = fromRandomPosition(nachnamen);
+    String kennung = vname.substring(0, 3) + nname.substring(0, 3) + (int) Math.random()*999;
+    return new BewerberModel(kennung, nname, vname, fromRandomPosition(geburtstage));
+  }
+
+  private String fromRandomPosition(String[] array) {
+    return array[(int) Math.random()*array.length];
+  }
+}
---
+++ src/test/java/mops/services/MappingServiceTest.java
@@ -0,0 +1,115 @@
+package mops.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Locale;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import mops.domain.database.dto.*;
+import mops.domain.models.*;
+import mops.domain.services.IMappingService;
+import mops.services.MappingService;
+
+@SpringBootTest
+public class MappingServiceTest {
+
+  private transient IMappingService mappingService;
+
+  @BeforeEach
+  void setUp() {
+    mappingService = new MappingService();
+
+  }
+
+  @Test
+  public void bestandeneModuleDTOZuBestandeneModuleModel() {
+    //TODO: create a generator for this
+    //TODO: differentiate DTO embeddables from models
+    ModulDTO modulDTO = new ModulDTO("RA", "golov@hhu.de", "janine golov");
+
+    BestandeneModuleDTO bestandeneModuleDTO = new BestandeneModuleDTO();
+    bestandeneModuleDTO.setNote(1.3);
+    bestandeneModuleDTO.setModul(modulDTO);
+    System.out.println(mappingService);
+    BestandeneModule bestandeneModule = mappingService.load(bestandeneModuleDTO);
+    Modul modul = bestandeneModule.getModul();
+
+    assertNotNull(bestandeneModule);
+    assertNotNull(modul);
+
+    //are those checks below actually nessecary? not sure
+    assertEquals(bestandeneModule.getNote(), 1.3);
+    assertEquals(modulDTO.getModul(), modul.getModulName());
+  }
+
+  /**
+   * Make sure the mappingservice
+   * does not break when passing null
+   */
+  @Test
+  public void bestandenesModulDTOIsNullMappingReturnsNull() {
+    BestandeneModuleDTO bestandeneModuleDTO = null;
+    BestandeneModule bestandeneModule = mappingService.load(bestandeneModuleDTO);
+
+    assertNull(bestandeneModule);
+  }
+
+  @Test
+  public void personalienDTOZuPersonalienModel() {
+    AdresseDTO adresseDTO = new AdresseDTO();
+    adresseDTO.setHausnummer("11a");
+    adresseDTO.setPLZ("40233");
+    adresseDTO.setStraße("Simrockstr");
+    adresseDTO.setWohnort("Düsseldorf");
+
+    PersonalienDTO personalienDTO = new PersonalienDTO();
+    personalienDTO.setAdresse(adresseDTO);
+    personalienDTO.setAlter(20);
+    try{
+      personalienDTO.setGeburtsdatum(Date.from(new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).parse("15.07.1999").toInstant()));
+    }catch(Exception e){
+      personalienDTO.setGeburtsdatum(null);
+    }
+    personalienDTO.setGeburtsort("Swetlana");
+    personalienDTO.setName("Wick");
+    personalienDTO.setNationalitaet("Terminator");
+    personalienDTO.setVorname("John");
+    personalienDTO.setUnikennung("johwi200");
+
+    Personalien personalien = mappingService.load(personalienDTO);
+
+    Adresse adresse = personalien.getAdresse();
+
+    assertNotNull(personalien);
+    assertNotNull(adresse);
+
+    assertEquals(adresseDTO.getHausnummer(), adresse.getHausnummer());
+    assertEquals(adresseDTO.getPLZ(), adresse.getPLZ());
+    assertEquals(adresseDTO.getStraße(), adresse.getStraße());
+    assertEquals(adresseDTO.getWohnort(), adresse.getWohnort());
+
+    assertEquals(personalienDTO.getAlter(), personalien.getAlter());
+    assertEquals(personalienDTO.getGeburtsdatum(), personalien.getGeburtsdatum());
+    assertEquals(personalienDTO.getGeburtsort(), personalien.getGeburtsort());
+    assertEquals(personalienDTO.getName(), personalien.getName());
+    assertEquals(personalienDTO.getNationalitaet(), personalien.getNationalitaet());
+    assertEquals(personalienDTO.getUnikennung(), personalien.getUnikennung());
+    assertEquals(personalienDTO.getVorname(), personalien.getVorname());
+  }
+
+  @Test
+  public void personalienDTOIsNullMappingReturnsNull() {
+    PersonalienDTO personalienDTO = null;
+    Personalien personalien = mappingService.load(personalienDTO);
+
+    assertNull(personalien);
+  }
+}
