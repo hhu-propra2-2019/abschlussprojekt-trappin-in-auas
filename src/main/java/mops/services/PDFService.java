3dbda3ec07347b7ded18b54cf3f0581f2f81fc48
heyoka95 <akin.hero@hotmail.de>
2020-03-16 13:28:20 +0100

    Fehlerbehandlung debudded

--- src/main/java/mops/services/PDFService.java
+++ src/main/java/mops/services/PDFService.java
@@ -23,48 +23,103 @@ public class PDFService {
 
 
 
-    public void convertWissenHilfskraft(BewerberDTO bewerberDTO) throws IOException {
-        PDDocument pDDocument = PDDocument.load(new File("/home/heyoka/Schreibtisch/progra2/projekt/abschlussprojekt-trappin-in-auas/3_Antrag_Beschaeftigung_wiss_Hilfskraefte_mit_BA.pdf"));
-        PDAcroForm pDAcroForm = pDDocument.getDocumentCatalog().getAcroForm();
+    public void fillStudentHilfskraft(BewerberDTO bewerberDTO) throws Exception {
 
-        PDField field = pDAcroForm.getField("Vorname");
-        field.setValue(bewerberDTO.getPersonalienDTO().getVorname());
-        field = pDAcroForm.getField("Name");
-        field.setValue(bewerberDTO.getPersonalienDTO().getName());
-        field = pDAcroForm.getField("Geburtsdatum");
-        field.setValue(bewerberDTO.getPersonalienDTO().getGeburtsdatum());
-        field = pDAcroForm.getField("Staatsangehörigkeit");
-        field.setValue(bewerberDTO.getPersonalienDTO().getNationalitaet());
-        field = pDAcroForm.getField("Anschrift(Straße)");
-        field.setValue(bewerberDTO.getPersonalienDTO().getAdresseDTO().getStraße());
-        field = pDAcroForm.getField("Anschrift(Hausnummer)");
-        field.setValue(bewerberDTO.getPersonalienDTO().getAdresseDTO().getHausnummer());
-        field = pDAcroForm.getField("Anschrift(PLZ)");
-        field.setValue(bewerberDTO.getPersonalienDTO().getAdresseDTO().getPLZ());
-        field = pDAcroForm.getField("Anschrift(ORT)");
-        field.setValue(bewerberDTO.getPersonalienDTO().getAdresseDTO().getWohnort());
+        try {
+            PDDocument pDDocument = PDDocument.load(new File("/home/heyoka/Schreibtisch/progra2/projekt/abschlussprojekt-trappin-in-auas/321_Antrag_Beschaeftigung_stud_Hilfskraefte.pdf"));
 
 
-        field = pDAcroForm.getField("Neueinstellung");
-        field.setValue(bewerberDTO.getPraeferenzenDTO().getEinstiegtyp());
-        field = pDAcroForm.getField("Weiterbeschäftigung");
-        field.setValue(bewerberDTO.getPraeferenzenDTO().getEinstiegtyp());
-        field = pDAcroForm.getField("Arbeitszeit");
-        field.setValue(bewerberDTO.getPraeferenzen().getMinWunschStunden() + " - " + bewerberDTO.getPraeferenzen().getMinWunschStunden().getMaxWunschStunden());
-        field = pDAcroForm.getField("Bemerkung zum Antrag");
-        field.setValue(bewerberDTO.getPraeferenzenDTO().getKommentar() + "/n" + bewerberDTO.getPraeferenzenDTO().getEinschraenkungen() );
+            if (pDDocument.isEncrypted()) {
+                try {
+                    pDDocument.setAllSecurityToBeRemoved(true);
+                } catch (Exception e) {
+                    throw new Exception("The document is encrypted, and we can't decrypt it.", e);
+                }
+            }
 
 
+            //befuellen der Datei
+            PDAcroForm pDAcroForm = pDDocument.getDocumentCatalog().getAcroForm();
+            PDField field = pDAcroForm.getField("Vorname");
+            field.setValue(bewerberDTO.getPersonalienDTO().getVorname());
+            field = pDAcroForm.getField("Name");
+            field.setValue(bewerberDTO.getPersonalienDTO().getName());
+            field = pDAcroForm.getField("Geburtsdatum");
+            field.setValue(bewerberDTO.getPersonalienDTO().getGeburtsdatum());
+            field = pDAcroForm.getField("Staatsangehörigkeit");
+            field.setValue(bewerberDTO.getPersonalienDTO().getNationalitaet());
+            field = pDAcroForm.getField("Anschrift(Straße)");
+            field.setValue(bewerberDTO.getPersonalienDTO().getAdresseDTO().getStraße());
+            field = pDAcroForm.getField("Anschrift(Hausnummer)");
+            field.setValue(bewerberDTO.getPersonalienDTO().getAdresseDTO().getHausnummer());
+            field = pDAcroForm.getField("Anschrift(PLZ)");
+            field.setValue(bewerberDTO.getPersonalienDTO().getAdresseDTO().getPLZ());
+            field = pDAcroForm.getField("Anschrift(ORT)");
+            field.setValue(bewerberDTO.getPersonalienDTO().getAdresseDTO().getWohnort());
 
 
-        field = pDAcroForm.getField("Immatrikulation");
-        field.setValue(bewerberDTO.getKarriereDTO().getImmartikulationsStatus().getStatus());
-        field = pDAcroForm.getField("Studiengang");
-        field.setValue(bewerberDTO.getKarriereDTO().getImmartikulationsStatus().getFachrichtung());
+            field = pDAcroForm.getField("Neueinstellung");
+            field.setValue(bewerberDTO.getPraeferenzenDTO().getEinstiegtyp());
+            field = pDAcroForm.getField("Weiterbeschäftigung");
+            field.setValue(bewerberDTO.getPraeferenzenDTO().getEinstiegtyp());
+            field = pDAcroForm.getField("Arbeitszeit");
+            field.setValue(bewerberDTO.getPraeferenzen().getMinWunschStunden() + " - " + bewerberDTO.getPraeferenzen().getMinWunschStunden().getMaxWunschStunden());
+            field = pDAcroForm.getField("Immatrikulation");
+
+            field.setValue(bewerberDTO.getKarriereDTO().getImmartikulationsStatus().getStatus());
+            field = pDAcroForm.getField("Studiengang");
+            field.setValue(bewerberDTO.getKarriereDTO().getImmartikulationsStatus().getFachrichtung());
+            field = pDAcroForm.getField("Bemerkung zum Antrag");
+            field.setValue(bewerberDTO.getKarriereDTO().getArbeitserfahrung() + "/n" + bewerberDTO.getPraeferenzenDTO().getKommentar() + "/n" + bewerberDTO.getPraeferenzenDTO().getEinschraenkungen() + "/n" + bewerberDTO.getPraeferenzenDTO().getTutorenSchulungTeilnahme());
+
+            //Speichern der Datei
+            pDDocument.save("/home/heyoka/Schreibtisch/progra2/projekt/abschlussprojekt-trappin-in-auas/output.pdf");
+            pDDocument.close();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
 
 
 
     }
 
+    public void fillWissenHilfskraft(BewerberDTO bewerberDTO) throws Exception {
+
+        try {
+            PDDocument pDDocument = PDDocument.load(new File("/home/heyoka/Schreibtisch/progra2/projekt/abschlussprojekt-trappin-in-auas/323_Antrag_Beschaeftigung_wiss_Hilfskraefte_mit_BA.pdf"));
+
+
+            if (pDDocument.isEncrypted()) {
+                try {
+                    pDDocument.setAllSecurityToBeRemoved(true);
+                } catch (Exception e) {
+                    throw new Exception("The document is encrypted, and we can't decrypt it.", e);
+                }
+            }
+
+            //befuellen der Datei
+
+
+
+
+
+
+
+
+
+            //Speichern der Datei
+            pDDocument.save("/home/heyoka/Schreibtisch/progra2/projekt/abschlussprojekt-trappin-in-auas/output2t.pdf");
+            pDDocument.close();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
 
 }
