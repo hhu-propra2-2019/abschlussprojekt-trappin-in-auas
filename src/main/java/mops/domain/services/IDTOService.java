e512990629d5007e79e45dc13cfc1b0dcfc4de0d
AcnoZed <Me.ouss.am@outlook.de>
2020-03-16 12:57:16 +0100

    Interface Funktionen hinzugefuegt

--- src/main/java/mops/domain/services/IDTOService.java
+++ src/main/java/mops/domain/services/IDTOService.java
@@ -1,5 +1,11 @@
 package mops.domain.services;
 
-public interface IDTOService {
+import mops.domain.database.dto.*;
+import mops.domain.models.*;
 
+public interface IDTOService {
+    KarriereDTO load(Karriere karriere);
+    PersonalienDTO load(Personalien personalien);
+    PraeferenzenDTO load(Praeferenzen praeferenzen);
+    BewerberDTO load(Bewerber bewerber);
 }
