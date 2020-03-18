7d7974675094306cd170ec296b2b3fc2be704ea8
Jakob Handke <jakobhandke@icloud.com>
2020-03-18 10:24:52 +0100

    rework roles in BossController

--- src/main/java/mops/controller/BossController.java
+++ src/main/java/mops/controller/BossController.java
@@ -1,17 +1,18 @@
 package mops.controller;
 
-import mops.domain.models.Modul;
+import static mops.authentication.account.keycloak.KeycloakRoles.ROLE_BOSS;
 
+import mops.domain.models.Modul;
 import mops.services.ModelService;
 import mops.services.ModulService;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/bewerbung1/boss")
@@ -29,7 +30,7 @@ public class BossController {
    * @param token injected, present, if user is logged in
    * @return modulesetup html template
    */
-  //@Secured("ROLE_boss")
+  @Secured(ROLE_BOSS)
   @GetMapping("/modules")
   public String getModule(Model m, KeycloakAuthenticationToken token) {
     m.addAttribute("modul", new Modul());
@@ -44,11 +45,10 @@ public class BossController {
    * @param modul Modul, added to DB
    * @return redirect to modules
    */
-  //@Secured("ROLE_boss")
+  @Secured(ROLE_BOSS)
   @PostMapping("/postmodule")
   public String addModule(Model m, KeycloakAuthenticationToken token, Modul modul) {
     modulService.addModul(mappingService.loadModulDTO(modul));
     return "redirect:/bewerbung1/boss/modules";
   }
-
 }
