7169f08f12ded6b9a202962ad1edcdcb7f85170d
Jakob Handke <jakobhandke@icloud.com>
2020-03-18 10:25:10 +0100

    rework roles in OrgaController

--- src/main/java/mops/controller/OrgaController.java
+++ src/main/java/mops/controller/OrgaController.java
@@ -1,23 +1,21 @@
 package mops.controller;
 
-import mops.services.ModulService;
+import static mops.authentication.account.keycloak.KeycloakRoles.ROLE_ORGA;
+
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/bewerbung1/orga")
 public class OrgaController {
 
   @GetMapping("/verwaltung")
-  @Secured("ROLE_orga")
+  @Secured(ROLE_ORGA)
   public String getBewerbungsVerwaltung(Model m, KeycloakAuthenticationToken token) {
     KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
     String dozentmail = principal.getKeycloakSecurityContext().getIdToken().getEmail();
