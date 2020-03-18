3a8242abf8fce19ac7bd00a02426556cc7df2c31
bayarabdullah <bayar1996@gmail.com>
2020-03-18 12:38:38 +0100

    Test zur Methode loadModulList() geschrieben, alle Tests funktionieren jetzt

--- src/main/java/mops/domain/services/IModelSerice.java
+++ src/main/java/mops/domain/services/IModelSerice.java
@@ -3,11 +3,14 @@ package mops.domain.services;
 import mops.domain.database.dto.*;
 import mops.domain.models.*;
 
+import java.util.List;
+
 public interface IModelSerice {
     Karriere load(KarriereDTO karriereDTO);
     ModulAuswahl load(ModulAuswahlDTO modulAuswahlDTO);
     Personalien load(PersonalienDTO personalienDTO);
     Praeferenzen load(PraeferenzenDTO praeferenzenDTO);
-    public Modul loadModul(ModulDTO modulDTO);
-    public StudiengangAbschluss load(StudiengangAbschlussDTO fachAbschluss);
+    Modul loadModul(ModulDTO modulDTO);
+    StudiengangAbschluss load(StudiengangAbschlussDTO fachAbschluss);
+    List<Modul> loadModulList(List<ModulDTO> dtoList);
 }
