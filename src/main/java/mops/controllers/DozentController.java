3ada04d11405d0f5db439deca794df2a729108dc
Jakob Handke <jakobhandke@icloud.com>
2020-03-18 10:25:39 +0100

    rework roles in DozentController

--- src/main/java/mops/controllers/DozentController.java
+++ src/main/java/mops/controllers/DozentController.java
@@ -1,13 +1,12 @@
 package mops.controllers;
 
+import static mops.authentication.account.keycloak.KeycloakRoles.ROLE_ORGA;
+
 import java.util.List;
 import java.util.stream.Collectors;
-
 import mops.domain.database.dto.BewerberDTO;
 import mops.services.BewerberService;
-
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
-
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,13 +19,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DozentController {
 
     private transient BewerberService bewerberService;
-    private transient final String orgaRole = "ROLE_orga";
 
     public DozentController(BewerberService bewerberService){
       this.bewerberService = bewerberService;
     }
 
-    @Secured({orgaRole})
+    @Secured(ROLE_ORGA)
     @GetMapping("/uebersicht")
     public String verteilen(Model model, KeycloakAuthenticationToken token){
         List<BewerberDTO> alleBewerber = bewerberService.findAlleBewerber();
@@ -42,7 +40,7 @@ public class DozentController {
     return "dozent/uebersicht";
   }
 
-    @Secured({orgaRole})
+    @Secured(ROLE_ORGA)
     @GetMapping("/uebersicht/offene")
     public String offeneUebersicht(Model model, KeycloakAuthenticationToken token){
         List<BewerberDTO> offeneBewerbungen = bewerberService.findNichtVerteilt();
@@ -52,7 +50,7 @@ public class DozentController {
     return "";
   }
 
-    @Secured({orgaRole})
+    @Secured(ROLE_ORGA)
     @GetMapping("/uebersicht/zugewiesene")
     public String zugewieseneUebersicht(Model model, KeycloakAuthenticationToken token){
         List<BewerberDTO> zugewiesene = bewerberService.findVerteilt();
@@ -62,7 +60,7 @@ public class DozentController {
     return "";
   }
 
-    @Secured({orgaRole})
+    @Secured(ROLE_ORGA)
     @GetMapping("/uebersicht/detail/{kennung}")
     public String detailAnsicht(Model model, KeycloakAuthenticationToken token, @PathVariable String kennung){
         BewerberDTO bewerber = bewerberService.findBewerberByKennung(kennung);
