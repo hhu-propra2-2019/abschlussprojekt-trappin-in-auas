65e440bc4836322b576a9d7248ed560cb01b8596
Yousef Alizadeh <y.alizadeh98@me.com>
2020-03-19 13:01:26 +0100

    Service und Controller für den ModulSetup

--- src/main/java/mops/controllers/ModulSetupController.java
+++ src/main/java/mops/controllers/ModulSetupController.java
@@ -1,10 +1,12 @@
 package mops.controllers;
 
+import mops.domain.models.Modul;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import mops.services.ModulService;
@@ -18,6 +20,8 @@ public class ModulSetupController {
 
     @GetMapping("/modulsetup")
     public String modulsetup(Model m) {
+        m.addAttribute("modules", modulService.findAllModule());
+        m.addAttribute("modul", new Modul());
         return "boss/modulsetup";
     }
 
@@ -25,5 +29,10 @@ public class ModulSetupController {
     public String getModule(Model m, KeycloakAuthenticationToken token) {
         m.addAttribute("modules", modulService.findAllModule());
         return "boss/moduleEinsehen";
+    }
+    @PostMapping("/postmodule")
+    public String addModule(Model m, KeycloakAuthenticationToken token, Modul modul) {
+        modulService.addModul(modul);
+        return "redirect:./modulsetup";
     }
 }
