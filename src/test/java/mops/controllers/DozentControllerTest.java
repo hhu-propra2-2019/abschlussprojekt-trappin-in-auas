db39ceab381df064b6880dc75c263862eda80270
Jakob Handke <jakobhandke@icloud.com>
2020-03-19 09:59:46 +0100

    add first auth tests

--- src/test/java/mops/controllers/DozentControllerTest.java
+++ src/test/java/mops/controllers/DozentControllerTest.java
@@ -1,13 +1,14 @@
 package mops.controllers;
 
-import static org.mockito.Mockito.when;
-
 import com.c4_soft.springaddons.test.security.context.support.WithMockKeycloackAuth;
 import mops.Bewerbung1Application;
 import mops.domain.database.dto.*;
-import mops.domain.models.*;
+import mops.domain.models.Beruf;
+import mops.domain.models.EinstiegTyp;
+import mops.domain.models.TutorenSchulungTeilnahme;
 import mops.services.BewerberService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +18,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
+
+import static mops.authentication.account.keycloak.KeycloakRoles.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
@@ -34,6 +41,10 @@ class DozentControllerTest {
   private BewerberService bewerberService;
 
   private BewerberDTO bewerberDTO;
+
+  private String dozentViewGetUri = "/dozent/uebersicht";
+  private String dozentViewOpenGetUri = "/dozent/uebersicht/offene";
+  private String dozentViewClosedGetUri = "/dozent/uebersicht/zugewiesene";
 
   @BeforeEach
   void setUp() {
@@ -53,10 +64,84 @@ class DozentControllerTest {
     when(bewerberService.findVerteilt()).thenReturn(Collections.emptyList());
   }
 
-  @WithMockKeycloackAuth("orga")
+  // GET UEBERSICHT //
+
+  @Disabled // wegen falschem template
+  @WithMockKeycloackAuth("ROLE_orga")
   @Test
-  void testUebersichtWithAuth() {
+  void testUebersichtWithCorrectRole() throws Exception {
+    mockMvc.perform(get(dozentViewGetUri)).andExpect(status().isOk());
+  }
 
+  @WithMockKeycloackAuth(ROLE_BOSS)
+  @Test
+  void testUebersichtWithBossRole() throws Exception {
+    mockMvc.perform(get(dozentViewGetUri)).andExpect(status().is(403));
+  }
 
+  @WithMockKeycloackAuth(ROLE_STUDENT)
+  @Test
+  void testUebersichtWithStudentRole() throws Exception {
+    mockMvc.perform(get(dozentViewGetUri)).andExpect(status().is(403));
+  }
+
+  @WithMockKeycloackAuth(ROLE_VERTEILER)
+  @Test
+  void testUebersichtWithVerteilerRole() throws Exception {
+    mockMvc.perform(get(dozentViewGetUri)).andExpect(status().is(403));
+  }
+
+  // GET OFFENE //
+
+  @Disabled // wegen falschem template
+  @WithMockKeycloackAuth(ROLE_ORGA)
+  @Test
+  void testOffeneWithOrgaRole() throws Exception {
+    mockMvc.perform(get(dozentViewOpenGetUri)).andExpect(status().isOk());
+  }
+
+  @WithMockKeycloackAuth(ROLE_STUDENT)
+  @Test
+  void testOffeneWithStudentRole() throws Exception {
+    mockMvc.perform(get(dozentViewOpenGetUri)).andExpect(status().is(403));
+  }
+
+  @WithMockKeycloackAuth(ROLE_BOSS)
+  @Test
+  void testOffeneWithBossRole() throws Exception {
+    mockMvc.perform(get(dozentViewOpenGetUri)).andExpect(status().is(403));
+  }
+
+  @WithMockKeycloackAuth(ROLE_VERTEILER)
+  @Test
+  void testOffeneWithVerteilerRole() throws Exception {
+    mockMvc.perform(get(dozentViewOpenGetUri)).andExpect(status().is(403));
+  }
+
+  // GET ZUGEWIESEN
+
+  @Disabled // wegen falschem template
+  @WithMockKeycloackAuth(ROLE_ORGA)
+  @Test
+  void testZugewieseneWithOrgaRole() throws Exception {
+    mockMvc.perform(get(dozentViewClosedGetUri)).andExpect(status().isOk());
+  }
+
+  @WithMockKeycloackAuth(ROLE_VERTEILER)
+  @Test
+  void testZugewieseneWithVerteilerRole() throws Exception {
+    mockMvc.perform(get(dozentViewClosedGetUri)).andExpect(status().is(403));
+  }
+
+  @WithMockKeycloackAuth(ROLE_BOSS)
+  @Test
+  void testZugewieseneWithBossRole() throws Exception {
+    mockMvc.perform(get(dozentViewClosedGetUri)).andExpect(status().is(403));
+  }
+
+  @WithMockKeycloackAuth(ROLE_STUDENT)
+  @Test
+  void testZugewieseneWithStudentRole() throws Exception {
+    mockMvc.perform(get(dozentViewClosedGetUri)).andExpect(status().is(403));
   }
 }
