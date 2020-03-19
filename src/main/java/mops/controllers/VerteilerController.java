d8105d4bf2bff621997b1abb330f386ed2c5953d
Jakob Handke <jakobhandke@icloud.com>
2020-03-19 12:42:01 +0100

    verteiler template

--- src/main/java/mops/controllers/VerteilerController.java
+++ src/main/java/mops/controllers/VerteilerController.java
@@ -1,9 +1,12 @@
 package mops.controllers;
 
-import static mops.authentication.account.keycloak.KeycloakRoles.ROLE_ORGA;
+import static mops.authentication.account.keycloak.KeycloakRoles.ROLE_VERTEILER;
 
 import mops.domain.database.dto.BewerberDTO;
+import mops.domain.database.dto.ModulDTO;
 import mops.services.BewerberService;
+import mops.services.ModelService;
+import mops.services.ModulService;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
@@ -13,26 +16,38 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
-@RequestMapping("/verteiler")
+@RequestMapping("/bewerbung1/verteiler")
 public class VerteilerController {
 
   private transient BewerberService bewerberService;
+  private transient ModulService modulService;
 
-  public VerteilerController(BewerberService bewerberService){
+  public VerteilerController(BewerberService bewerberService, ModulService modulService) {
     this.bewerberService = bewerberService;
+    this.modulService = modulService;
   }
     
-  @Secured(ROLE_ORGA)
+  @Secured(ROLE_VERTEILER)
   @GetMapping("/uebersicht")
-  public String verteilen(Model model, KeycloakAuthenticationToken token){
-    //List<BewerberDTO> offeneBewerbungen = bewerberService.findNichtVerteilt();
-    //List<BewerberDTO> zugewieseneBewerbungen = bewerberService.findVerteilt();
-    //List<BewerberDTO> offeneBewerbungenPreview = offeneBewerbungen.stream().limit(5).collect(Collectors.toList());
-    return "verteiler/verteilerMainpage";
+  public String verteilen(Model model, KeycloakAuthenticationToken token) {
+    List<BewerberDTO> offeneBewerbungen = bewerberService.findNichtVerteilt();
+    List<BewerberDTO> zugewieseneBewerbungen = bewerberService.findVerteilt();
+    List<BewerberDTO> offeneBewerbungenPreview = offeneBewerbungen.stream().limit(5).collect(Collectors.toList());
+
+    model.addAttribute("offeneBewerbungen", offeneBewerbungen);
+    model.addAttribute("zugewieseneBewerbungen", zugewieseneBewerbungen);
+    model.addAttribute("offeneBewerbungenPreview", offeneBewerbungenPreview);
+    model.addAttribute("alleModule", modulService.findAllModule());
+    model.addAttribute("modul", new ModulDTO());
+
+    return "verteiler/Verteiler";
   }
 
-  @Secured(ROLE_ORGA)
+  @Secured(ROLE_VERTEILER)
   @PostMapping("/verteilung")
   public String verteilDetails(Model m, @RequestParam String kennung) {
     System.out.println("print: " + kennung);
@@ -45,5 +60,13 @@ public class VerteilerController {
     m.addAttribute("bewerber", bewerber);
     m.addAttribute("verteiltan", "");
     return "verteilungsdetails";
+  }
+
+  @Secured(ROLE_VERTEILER)
+  @PostMapping("/verteile")
+  public String verteile(Model m, @RequestParam String bewerberKennung, ModulDTO modulDTO) {
+    ModelService modelService = new ModelService();
+    bewerberService.verteile(bewerberKennung, modelService.loadModul(modulDTO).getDozent());
+    return "redirect:/bewerbung1/verteiler/uebersicht";
   }
 }
