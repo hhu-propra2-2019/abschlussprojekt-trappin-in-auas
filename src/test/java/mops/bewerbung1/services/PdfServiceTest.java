45a34bfc4d855c09c99d4adc6f9d1517a7e14179
heyoka95 <akin.hero@hotmail.de>
2020-03-20 10:11:43 +0100

    Tests für die PDF ZuOrdnung geschrieben

--- src/test/java/mops/bewerbung1/services/PdfServiceTest.java
+++ src/test/java/mops/bewerbung1/services/PdfServiceTest.java
@@ -45,7 +45,7 @@ public class PdfServiceTest {
         ImmartikulationsStatus immartikulationsStatus = new ImmartikulationsStatus(true,"Informatik");
         Karriere karriere = new Karriere("war Praktikant",immartikulationsStatus ,null,null);
         Praeferenzen praeferenzen = new Praeferenzen(15,20,null,null,EinstiegTyp.NEUEINSTIEG,null,null,TutorenSchulungTeilnahme.NICHTTEILNAHME);
-        BewerberDTO bewerberDTO = new BewerberDTO(karriere,personalien,praeferenzen);
+        //BewerberDTO bewerberDTO = new BewerberDTO(karriere,personalien,praeferenzen); To DO
         PDFService pdfService = new PDFService();
 
 
@@ -65,10 +65,32 @@ public class PdfServiceTest {
 
 
     @Test
-    public void pruefeFachAbschluss() throws ParseException {
+    public void pruefeFachAbschluss() throws Exception {
 
 
-     
+        //Arrange
+        Date geburtsdatum = new Date();
+        SimpleDateFormat ft = new SimpleDateFormat("dd.MM.yyyy");
+        String datum = "2.10.1995";
+        geburtsdatum = ft.parse(datum);
+        Adresse a = new Adresse("40789","Monheim","Tegeler Straße","15");
+        Personalien personalien = new Personalien(a,"1234","Akin","Kilincarslan",geburtsdatum,24,"Leverkusen","Deutsch");
+        ImmartikulationsStatus immartikulationsStatus = new ImmartikulationsStatus(true,"Informatik");
+        StudiengangAbschluss studiengangAbschluss = new StudiengangAbschluss("Informatik","Bachelor");
+        Karriere karriere = new Karriere("war Praktikant",immartikulationsStatus,studiengangAbschluss,null);
+        Praeferenzen praeferenzen = new Praeferenzen(15,20,null,null,EinstiegTyp.NEUEINSTIEG,null,null,TutorenSchulungTeilnahme.NICHTTEILNAHME);
+        //BewerberDTO bewerberDTO = new BewerberDTO(karriere,personalien,praeferenzen); TO DO
+        PDFService pdfService = new PDFService();
+
+
+        //Act
+        String pruefeFachabschluss = pdfService.filedirectory(bewerberDTO);
+        String AntragOhneFachAbschluss = "/home/heyoka/Schreibtisch/progra2/projekt/abschlussprojekt-trappin-in-auas/323_Antrag_Beschaeftigung_wiss_Hilfskraefte_mit_BA.pdf";
+
+
+        //Assert
+        Assert.assertEquals(AntragOhneFachAbschluss, pruefeFachabschluss);
+
 
 
 
