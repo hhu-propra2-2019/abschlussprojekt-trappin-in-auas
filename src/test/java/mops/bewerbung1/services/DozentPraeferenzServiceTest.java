d5b17f3a05053b9ad07fa44ee561b70d6a73131b
AcnoZed <Me.ouss.am@outlook.de>
2020-03-24 10:30:06 +0100

    In Zusammenarbeit mit Akin: Tests laufen nun ohne Porstgres-DB H2 dependency hinzugefuegt, Tests annotiert

--- src/test/java/mops/bewerbung1/services/DozentPraeferenzServiceTest.java
+++ src/test/java/mops/bewerbung1/services/DozentPraeferenzServiceTest.java
@@ -12,9 +12,13 @@ import mops.services.DozentPraeferenzService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 
 @SpringBootTest
+
 public class DozentPraeferenzServiceTest {
   private transient final String BEWERBER = "bewerber";
   private transient final String DOZENT_MAIL = "dozentMail";
