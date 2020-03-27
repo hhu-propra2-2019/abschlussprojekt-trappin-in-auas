463b459340584132f68df4a29d5128a05dfcc7ee
Jakob Handke <jakobhandke@icloud.com>
2020-03-27 12:12:03 +0100

    move BewerbungErrorController

--- src/main/java/mops/controller/BewerbungErrorController.java
+++ src/main/java/mops/controller/BewerbungErrorController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.RequestDispatcher;
